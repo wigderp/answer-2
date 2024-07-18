31a50b605e31440b88728ec4312ca02316caa575
wigderp <main.wigderp@gmail.com>
2024-07-18 17:59:25 +0300

    modified:   main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ categories_df = spark.read.format("csv").options(header="true").load("path_to_ca
 # Выполнение объединения продуктов по общему ключу
 joined_df = products_df.join(categories_df, products_df.product_id == categories_df.product_id, "left")
 
-# Вывод всех пар "Имя продукта - Имя категории"
+#"Имя продукта - Имя категории"
 joined_df.select("product_name", "category_name").show()
 
-# Вывод имен всех продуктов, у которых нет категорий
+# Нет категорий
 products_without_category = joined_df.filter(col("category_name").isNull()).select("product_name")
 products_without_category.show()
